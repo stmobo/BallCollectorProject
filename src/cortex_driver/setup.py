b3a1441159faddd0dc6aebcfcf89f87539f3ce26
Sebastian Mobo <stmobo@gmail.com>
2018-03-01 16:04:11 -0600

    Restructure some stuff?

--- src/cortex_driver/setup.py
+++ src/cortex_driver/setup.py
@@ -3,7 +3,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
     packages=['cortex_driver'],
-    package_dir={'': ''},
+    package_dir={'': 'scripts'},
 )
 
 setup(**setup_args)
