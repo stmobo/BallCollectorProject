f5ed7e2f3e4597ae269ff947e8748a0ab4df477b
Sebastian Mobo <stmobo@gmail.com>
2018-03-01 15:53:22 -0600

    Revert "Rename cortex_driver package"
    This reverts commit 5b960dc52d20616f910ded7a74a5faa93d2e359a.

--- src/cortex_driver/setup.py
+++ src/cortex_driver/setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-    packages=['cortex_driver_node'],
-    package_dir={'cortex_driver_node': 'cortex_driver_node'},
+    packages=['cortex_driver'],
+    package_dir={'': ''},
 )
 
 setup(**setup_args)
