3f4234e592079232dd790b0fd5552db65523f1a8
Sebastian Mobo <stmobo@gmail.com>
2018-03-03 23:23:38 -0600

    Add speed commands to drive_console

--- src/cortex_driver/scripts/cortex_driver/drive_console.py
+++ src/cortex_driver/scripts/cortex_driver/drive_console.py
@@ -20,21 +20,34 @@ def main():
             break
         elif i == '':
             make_motor_call(0, 0)
-        elif i == 'f' or i == 'w':
-            make_motor_call(300, 300)
-        elif i == 'b' or i == 's':
-            make_motor_call(-300, -300)
-        elif i == 'l' or i == 'a':
-            make_motor_call(300, -300)
-        elif i == 'r' or i == 'd':
-            make_motor_call(-300, 300)
+        elif len(i) <= 2:
+            mult = 1
+            if len(i) == 2:
+                if i[1] == 'f':
+                    mult = 2
+                else:
+                    try:
+                        mult = 1 + ((int(i[1]) - 5) / 10)
+                    except ValueError as e:
+                        # ignore invalid values of i[1]
+                        mult = 1
+
+            if i[0] == 'f' or i[0] == 'w':
+                make_motor_call(300*mult, 300*mult)
+            elif i[0] == 'b' or i[0] == 's':
+                make_motor_call(-300*mult, -300*mult)
+            elif i[0] == 'l' or i[0] == 'a':
+                make_motor_call(300*mult, -300*mult)
+            elif i[0] == 'r' or i[0] == 'd':
+                make_motor_call(-300*mult, 300*mult)
         else:
             m = ptn.match(i)
 
-            m1 = int(m.group(1))
-            m2 = int(m.group(2))
+            if m is not None:
+                m1 = int(m.group(1))
+                m2 = int(m.group(2))
 
-            make_motor_call(m1, m2)
+                make_motor_call(m1, m2)
 
 if __name__ == '__main__':
     main()
